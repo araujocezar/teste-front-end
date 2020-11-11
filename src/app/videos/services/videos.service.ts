cea1a2b2f57a628079a83fa5458ffeff6c67a312
Cezar Rômulo Cordeiro de Araújo Filho <cezar.cordeiro@dce.ufpb.br>
2020-11-11 00:40:04 -0300

    feat: ligação com service de videos

--- src/app/videos/services/videos.service.ts
+++ src/app/videos/services/videos.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import axios, { AxiosResponse } from 'axios';
+import { environment } from 'src/environments/environment';
+import { Videos } from '../interfaces/videos.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideosService {
 
-  constructor() { }
+  constructor() {
+    this.registerBaseEndpoint(`${environment.api.endpoint}/`);
+   }
+
+  getVideos(searchString: string): Promise<AxiosResponse<Videos>> {
+    const param = {
+      part: environment.api.part,
+      q: searchString,
+      key: environment.api.key,
+    };
+    return axios.get('/search', {params: param});
+  }
+
+  private registerBaseEndpoint(endpoint: string) {
+    axios.defaults.baseURL = endpoint;
+  }
 }
